e992d3ce79e56b002b2d789ddeb73f0e09bb256a
vbhvtrpthi <51736266+vbhvtrpthi@users.noreply.github.com>
2022-10-16 12:00:40 +0530

    Time: 4 ms (81.38%), Space: 42.7 MB (80.65%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -14,78 +14,76 @@
  * }
  */
 class Solution {
-    class Pair implements Comparable<Pair> {
+    class Pair implements Comparable<Pair>{
         TreeNode node;
-        int w;
-        int d;
-
-        Pair(TreeNode node, int w, int d){
+        int h;
+        int v;
+        
+        Pair(TreeNode node, int h, int v){
             this.node = node;
-            this.w = w;
-            this.d = d;
+            this.h = h;
+            this.v = v;
         }
-
-        // this - other means increasing
-        // other - this means decreasing
+        
         public int compareTo(Pair other){
-            if(this.d == other.d){
+            if(this.v == other.v){
                 return this.node.val - other.node.val;
-            } else {
-                return this.d - other.d;
+            }else{
+                return this.v - other.v;
             }
         }
     }
-
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        Queue<Pair> queue = new LinkedList<>();
-        queue.add(new Pair(root, 0, 1));
-
-        int lmw = 0;
-        int rmw = 0;
+        Queue<Pair> q = new LinkedList<>();
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
         
-        HashMap<Integer, ArrayList<Pair>> map = new HashMap<>();
-        while(queue.size() > 0){
-            Pair temp = queue.remove();
-
-            if(temp.w < lmw){
-                lmw = temp.w;
+        q.add(new Pair(root, 0, 1));
+        
+        //for sorting from left to right
+        int lmh = 0;
+        int rmh = 0;
+        
+        HashMap<Integer, PriorityQueue<Pair>> map = new HashMap<>();
+        
+        while(q.size() > 0){
+            
+            Pair temp = q.remove();
+            
+            if(temp.h < lmh)
+                 lmh = temp.h;
+            
+            if(temp.h > rmh)
+                rmh = temp.h;
+            
+            if(map.containsKey(temp.h) == false){
+                map.put(temp.h, new PriorityQueue<>());
+                map.get(temp.h).add(temp);
+            }else{
+                map.get(temp.h).add(temp);
             }
-
-            if(temp.w > rmw){
-                rmw = temp.w;
-            }
-
-            if(map.containsKey(temp.w) == false){
-                map.put(temp.w, new ArrayList<>());
-                map.get(temp.w).add(temp);
-            } else {
-                map.get(temp.w).add(temp); 
-            }
-
+            
             if(temp.node.left != null){
-                queue.add(new Pair(temp.node.left, temp.w - 1, temp.d + 1));
+                q.add(new Pair(temp.node.left, temp.h - 1, temp.v + 1));
             }
-
+            
             if(temp.node.right != null){
-                queue.add(new Pair(temp.node.right, temp.w + 1, temp.d + 1));
+                q.add(new Pair(temp.node.right, temp.h + 1, temp.v + 1));
             }
         }
-
+        
         List<List<Integer>> res = new ArrayList<>();
-        for(int w = lmw; w <= rmw; w++){
-            ArrayList<Integer> list = new ArrayList<>();
-            ArrayList<Pair> unsortedList = map.get(w);
-            Collections.sort(unsortedList);
-
-            for(Pair temp: unsortedList){
+        for(int i = lmh; i <= rmh; i++){
+            List<Integer> list = new ArrayList<>();
+            PriorityQueue<Pair> prq = map.get(i);
+            
+            
+            while(prq.size() > 0){
+                Pair temp = prq.remove();
                 list.add(temp.node.val);
             }
-
-
+            
             res.add(list);
         }
-        
-
-        return res;
+      return res;
     }
 }
