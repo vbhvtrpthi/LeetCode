26a73bbddc7ae43d172f9f0c592d4eaa9f52a189
vbhvtrpthi <51736266+vbhvtrpthi@users.noreply.github.com>
2022-10-27 23:35:00 +0530

    Time: 1 ms (98.67%), Space: 44.1 MB (9.03%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -14,44 +14,45 @@
  * }
  */
 class Solution {
+
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        
-        List<List<Integer>> flist = new ArrayList<>();
-        Stack<List<Integer>> st = new Stack<>();
-        
-        if(root == null)
-             return flist;
-        
+        List<List<Integer>> ret = new ArrayList<>();
+        if (root == null) return ret;
         Queue<TreeNode> q = new LinkedList<>();
-        List<Integer> list = new ArrayList<>();
-        
+
         q.add(root);
         q.add(null);
-        
-        while(q.size() > 0){
-            TreeNode node = q.remove();
-            
-            if(node != null){
-                list.add(node.val);
-                
-                if(node.left != null)
-                     q.add(node.left);
-                if(node.right != null)
-                     q.add(node.right);
-            }else{
-                st.push(list);
-                list = new ArrayList<>();
-                if(q.size() > 0){
+
+        List<Integer> list = new ArrayList<>();
+
+        while (q.size() > 0) {
+            TreeNode temp = q.remove();
+            if (temp != null) list.add(temp.val);
+
+            if (temp != null && temp.left != null) {
+                q.add(temp.left);
+            }
+
+            if (temp != null && temp.right != null) {
+                q.add(temp.right);
+            }
+
+            if (temp == null) {
+                if (q.size() > 0) {
+                    ret.add(list);
+                    list = new ArrayList<>();
                     q.add(null);
+                } else {
+                    ret.add(list);
                 }
             }
         }
-        
 
-        while(st.size() > 0){
-            flist.add(st.pop());
+        List<List<Integer>> ans = new ArrayList<>();
+        for (List<Integer> val : ret) {
+            ans.add(0, val);
         }
-        
-        return flist;
+
+        return ans;
     }
 }
