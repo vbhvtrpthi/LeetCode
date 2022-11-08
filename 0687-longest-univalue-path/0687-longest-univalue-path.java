82fdd25f510a9b47ae2f9871305c2e2d87ab3f63
vbhvtrpthi <51736266+vbhvtrpthi@users.noreply.github.com>
2022-11-08 10:07:43 +0530

    Time: 3 ms (87.31%), Space: 68.9 MB (10.02%) - LeetHub

--- 0687-longest-univalue-path/0687-longest-univalue-path.java
+++ 0687-longest-univalue-path/0687-longest-univalue-path.java
@@ -14,29 +14,30 @@
  * }
  */
 class Solution {
-    int len;
-
+    int len = 0;
     public int longestUnivaluePath(TreeNode root) {
         len = 0;
         helper(root, -1);
-        if (len > 0) len = len - 1;
-
+        
+        if(len > 0)
+            len = len - 1;
+        
         return len;
     }
-
-    public int helper(TreeNode node, int val) {
-        if (node == null) return 0;
+    public int helper(TreeNode node, int val){
+        if(node == null)  return 0;
+        
         int left = helper(node.left, node.val);
         int right = helper(node.right, node.val);
-
-        if (left + right + 1 > len) {
-            len = left + right + 1;
-        }
-
-        if (val == node.val) {
-            return Math.max(left, right) + 1;
-        } else {
+        
+        if(left  + right + 1 > len)
+             len = left + right + 1;
+        
+        if(node.val == val){
+            return Math.max(left, right)  + 1;
+        }else{
             return 0;
         }
+        
     }
 }
